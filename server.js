d26f5fa548c8e6c0124abc3b0d072b55e46505da
samini1 <92944248+samini1@users.noreply.github.com>
2022-01-27 11:38:57 -0600

    files were not linking across index and notes html, reworked to relative paths so I could see what was supposed to be happening

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //modules required for server
-const express = require('expresss');
+const express = require('express');
 const fs = require('fs');
+// const { readFile } = require('fs/promises');
+
 //server setup
 const app = express();
 const PORT = process.env.PORT || 3001; 
@@ -8,31 +10,40 @@ app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 
 //array to put notes in
-const notesArr = [];
+const notesInput = [];
 
-//api routes
+// //api routes
 //get route to read db.json file and return saved notes as JSON
-app.get("/api/notes", function(req, res) {
+app.get("/api/notes", function(err, res) {
+    fs.readFile('./db/db.json')
 
+    res.send('Hello World');
 })
 
-//post route should recieve new node to save on the body,
-// add to db.json, return new note to client
-//each note should have unique id (look at npm packages)
-app.post('/api/notes', function(req, res) {
+// //post route should recieve new node to save on the body,
+// // add to db.json, return new note to client
+// //each note should have unique id (look at npm packages)
+// app.post('/api/notes', function(req, res) {
+//     notesInput = JSON.parse(notesInput);
+//     notesInput.push(req.body);
+//     notesInput = JSON.stringify(notesInput);
+//     fs.writeFile('./db/db.json', notesInput, "utf8");
+//     res.json(JSON.parse(notesInput));
+// })
 
-})
+// //html routes
+// //get /notes should return the notes.html
+// app.get('/notes', function(req, res) {
+//     res.sendFile('db.');
 
-//html routes
-//get /notes should return the notes.html
-app.get('/notes', function(req, res) {
-    res.sendFile()
+// });
 
-})
+// //GET * should return the index.html
+// app.get('*', function(req, res) {
 
-//GET * should return the index.html
-app.get('*', function(req, res) {
+// })
 
-})
-
-app.listen ()
+app.listen (PORT, function(err) {
+    if (err) console.log('server setup error')
+    console.log("Server listening on port", PORT);
+});
