c5eecf61e41077f86c240384dadfce125be27f62
samini1 <92944248+samini1@users.noreply.github.com>
2022-01-28 13:51:16 -0600

    replaced app with router const in html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 //html routes
 //get /notes should return the notes.html
-app.get('/notes', function(req, res) {
+router.get('/notes', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 
 });
 
 //GET * should return the index.html
-app.get('*', function(req, res) {
+router.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
