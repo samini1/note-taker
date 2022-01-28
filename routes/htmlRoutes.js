e7ffc8c12550bf571fd74de68c4031df7e8b2a48
samini1 <92944248+samini1@users.noreply.github.com>
2022-01-28 14:38:43 -0600

    attempting deployment

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,13 +3,13 @@ const path = require('path');
 //html routes
 //get /notes should return the notes.html
 router.get('/notes', function(req, res) {
-    res.sendFile(path.join('../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 
 });
 
 //GET * should return the index.html
 router.get('*', function(req, res) {
-    res.sendFile(path.join('../public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 module.exports = router;
